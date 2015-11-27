fff02cd97b3c32fe5c90c7807e0493f830e53c9b
Keegan Morrow <Korrow@gmail.com>
2015-11-27 14:51:55 -0800

    BspwmDesktop now sees urgent desktops
    Bspwm (should) draw urgent desktops with a different foreground color.
    Not tested yet, but doesn't seem to have broken anything else.

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ render = statusbar.modules.render.powerlinish.PowerlineRenderer()
 panel = statusbar.Panel('181818', '181818', render, 0.25)
 
 panel.add_segment(statusbar.ClockIndicator(properties={'bgcolor':'afff00', 'pl_left':True, 'pl_right': False, 'alignment':'right'}))
-panel.add_segment(statusbar.BspwmDesktops(properties={'bgcolor':'00afff', 'pl_left':False, 'pl_right': True, 'alignment':'left', 'fgcolor_active': 'ffffff', 'fgcolor_inactive': '181818'}))
+panel.add_segment(statusbar.BspwmDesktops(properties={'bgcolor':'00afff', 'pl_left':False, 'pl_right': True, 'alignment':'left', 'fgcolor_active': 'ffffff', 'fgcolor_inactive': '181818', 'fgcolor_urgent': 'ff0000'}))
 
 # panel.add_segment(statusbar.Segment(properties={'bgcolor':'afff00', 'pl_left':True, 'pl_right': True}))
 panel.execute()
--- statusbar/__init__.py
+++ statusbar/__init__.py
@@ -69,11 +69,13 @@ class BspwmDesktops(Segment):
         return
     def get_output(self):
         result = ''
-        pattern = re.compile(r':([O,o,F,f])(\d)')
+        pattern = re.compile(r':([O,o,F,f,U,u])(\d)')
         status_b = subprocess.check_output(['bspc', 'control', '--get-status'])
         status = status_b.decode('utf-8')
         for (letter, num) in re.findall(pattern, status):
-            if letter.isupper():
+            if letter.lower() == 'u':
+                result += ' %{{F#{fgcolor_urgent}}}{0} '.format(num,**self.properties)
+            elif letter.isupper():
                 result += ' %{{F#{fgcolor_active}}}{0} '.format(num,**self.properties)
             else:
                 result += ' %{{F#{fgcolor_inactive}}}{0} '.format(num,**self.properties)
